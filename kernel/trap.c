a47f3711262acb1c21bfeeceea883791d33bfcc7
vector <zijiebing@gmail.com>
2021-05-08 03:33:20 -0700

    Lazy allocation

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,19 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if(r_scause() == 13 || r_scause() == 15) {
+    // page fault
+    char *mem;
+    uint64 faultingVa = r_stval();
+    if(faultingVa >= p->sz || (mem = kalloc()) == 0) { 
+      p->killed = 1;
+    } else {
+      memset(mem, 0, PGSIZE);
+      if(mappages(p->pagetable, PGROUNDDOWN(faultingVa), PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0) {
+        kfree(mem);
+        p->killed = 1;
+      }
+    }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
