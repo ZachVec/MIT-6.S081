9a7f8949642a617e4806c89ebe2a044268eb4e7f
vector <zijiebing@gmail.com>
2021-05-19 04:27:18 -0700

    part1/Uthread: switch between threads done

--- user/uthread.c
+++ user/uthread.c
@@ -11,10 +11,32 @@
 #define MAX_THREAD  4
 
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved registers
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 struct thread {
+  // add the struct context at the very top of thread
+  // so that you don't have to calculate the offset of
+  // each register in context
+  struct     context context;   /* Thread Context */
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -63,6 +85,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)t, (uint64)next_thread);
   } else
     next_thread = 0;
 }
@@ -76,7 +99,13 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  // when thread_scheduler() runs a given thread for the first time
+  // the thread has to executes function `func`
+  t->context.ra = (uint64)func;
+  // on its own stack. 
+  // Here register sp points to the bottom of stack
+  // for the reason that stack grows downwards.
+  t->context.sp = (uint64)(t->stack + STACK_SIZE);
 }
 
 void 
